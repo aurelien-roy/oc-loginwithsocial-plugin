4004062395d3197af70a0e10de6344217cab677f
Aurelien Roy <aurelien_roy@outlook.com>
2020-03-01 12:41:50 +0100

    Spanish locale

---
+++ lang/frontend.php
@@ -0,0 +1,18 @@
+<?php return [
+    'login_button' => 'Continuar con :provider',
+    'continue_registration_with' => 'Terminar registro con :provider',
+    'auth_error_noname' => 'Un error a ocurrido durante la autentificación con el proveedor social.',
+    'auth_error' => 'Un error a ocurrido durante la autentificación con el proveedor :provider.',
+    'already_linked' => 'Ya tienes una cuenta de :provider vinculada.',
+    'no_account_linked' => 'No tienes cuenta de :provider vinculada.',
+    'autolink_disabled_error' => 'Ya tienes una cuenta asociada al e-mail :email pero no usaste :provider para crear la cuenta. Por favor, intenta iniciar sesión usando :alt_provider.',
+    'autolink_no_alternative_error' => 'Hay un problema con la cuenta asociada al e-mail :email. Por favor, intenta restablecer tu contraseña.',
+    'autolink_duplicate_error' => 'Ya tienes una cuenta asociada al e-mail :email, pero la tienes vinculada a otra cuenta de :provider. Por favor, intenta iniciar sesión usando otra cuenta de :provider.',
+    'expired_error' => 'La página ha expirado. Por favor, intenta iniciar sesión de nuevo.',
+    'linked_to_other' => 'Esta cuenta de :provider ya está vinculada a otra cuenta de :app.',
+    'already_logged_in' => 'Ya tienes la sesión iniciada.',
+    'link_account' => 'Vincular cuenta',
+    'unlink_account' => 'Desvincular cuenta',
+    'not_linked' => 'Sin vincular',
+    'linked_accounts' => 'Redes sociales vinculadas'
+];
