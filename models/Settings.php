7f0524e845d8004bcd875cb18decdc4d642fe18d
Aurélien Roy <aurelien_roy@outlook.com>
2020-04-07 15:41:04 +0200

    Clearer hints in Settings

--- models/Settings.php
+++ models/Settings.php
@@ -22,11 +22,17 @@ class Settings extends Model
                 $is_configured = $apm->isConfigured($p['name']);
                 $vars = $apm->getVariableNames($p['name']);
                 $url = route('tlokuus_loginwithsocial_callback', ['provider' => $p['name']]);
+                $name = $is_configured ? $p['name'] : ($p['name'] . ' (not configured)');
+
+                $tooltip = 'This is the callback/redirect URL you must give to the provider.';
+                if(!$is_configured) {
+                    $tooltip = "Add {$vars['id']} and {$vars['secret']} variables into your .env file to enable this provider. " . $tooltip;
+                }
                 
-                return [$p['name'], [
-                    'comment' => $is_configured ? $url : 'Not configured',
+                return [$name, [
+                    'comment' => $url,
                     'disabled' => !$is_configured,
-                    'tooltip' => $is_configured ? 'This is the callback/redirect URL you must give to the provider if required.' : "Add {$vars['id']} and {$vars['secret']} variables into your .env file to enable this provider.",
+                    'tooltip' => $tooltip
                 ]];
             })->sortBy(function($k){
                 return [$k[1]['disabled'], $k[0]];
